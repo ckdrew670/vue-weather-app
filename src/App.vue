3e4e4b57f5dd794d45c90ea569dcfcb0030c56de
Charlotte <ckdrew670@hotmail.com>
2021-12-16 12:29:50 +0000

    Add vue data

--- src/App.vue
+++ src/App.vue
@@ -24,23 +24,23 @@
         <!-- Background Images to Go Here -->
 
         <div class="card__header">
-          <h2 class="card__city">Bristol</h2>
-          <p class="card__country">UK</p>
+          <h2 class="card__city">{{ weather.cityName }}</h2>
+          <p class="card__country">{{ weather.country }}</p>
         </div>
 
         <div class="card__body">
-          <p class="card__temp">21&deg;C</p>
-          <p class="card__summary">Light rain</p>
+          <p class="card__temp">{{ weather.temp }}&deg;C</p>
+          <p class="card__summary">{{ weather.description }}</p>
         </div>
 
         <div class="card__footer">
           <div class="card__feels-like">
             <h3>Feels like</h3>
-            <p class="card__feels-like__temp">21&deg;C</p>
+            <p class="card__feels-like__temp">{{ weather.feelsLike }}&deg;C</p>
           </div>
           <div class="card__humidity">
             <h3>Humidity</h3>
-            <p class="card__humidity-percentage">70%</p>
+            <p class="card__humidity-percentage">{{ weather.humidity }}%</p>
           </div>
         </div>
       </section>
@@ -52,7 +52,18 @@
 
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      weather: {
+        cityName: "Exeter",
+        country: "UK",
+        temp: 3,
+        description: "Light Rain",
+        feelsLike: 2,
+        humidity: 30
+      }
+    };
+  }
 }
 </script>
 
